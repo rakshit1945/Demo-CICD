0e10bd777c9a3d271d8db834680882966bc31552
rakshit <rakshit1945@gmail.com>
2025-10-22 02:51:16 +0530

    The airflow DAG

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -24,8 +24,8 @@ with DAG(
 
     # Fetch environment variables
     env = Variable.get("env", default_var="dev")
-    gcs_bucket = Variable.get("gcs_bucket", default_var="airflow-projetcs-gds")
-    bq_project = Variable.get("bq_project", default_var="psyched-service-442305-q1")
+    gcs_bucket = Variable.get("gcs_bucket", default_var="airflow-projects-rj")
+    bq_project = Variable.get("bq_project", default_var="tough-pod-475707-m1")
     bq_dataset = Variable.get("bq_dataset", default_var=f"flight_data_{env}")
     tables = Variable.get("tables", deserialize_json=True)
 
@@ -41,7 +41,7 @@ with DAG(
     file_sensor = GCSObjectExistenceSensor(
         task_id="check_file_arrival",
         bucket=gcs_bucket,
-        object=f"airflow-project-1/source-{env}/flight_booking.csv",  # Full file path in GCS
+        object=f"source-{env}/flight_booking.csv",  # Full file path in GCS
         google_cloud_conn_id="google_cloud_default",  # GCP connection
         timeout=300,  # Timeout in seconds
         poke_interval=30,  # Time between checks
@@ -51,7 +51,7 @@ with DAG(
     # Task 2: Submit PySpark job to Dataproc Serverless
     batch_details = {
         "pyspark_batch": {
-            "main_python_file_uri": f"gs://{gcs_bucket}/airflow-project-1/spark-job/spark_transformation_job.py",  # Main Python file
+            "main_python_file_uri": f"gs://{gcs_bucket}/spark-job/spark_transformation_job.py",  # Main Python file
             "python_file_uris": [],  # Python WHL files
             "jar_file_uris": [],  # JAR files
             "args": [
@@ -68,9 +68,9 @@ with DAG(
         },
         "environment_config": {
             "execution_config": {
-                "service_account": "70622048644-compute@developer.gserviceaccount.com",
-                "network_uri": "projects/psyched-service-442305-q1/global/networks/default",
-                "subnetwork_uri": "projects/psyched-service-442305-q1/regions/us-central1/subnetworks/default",
+                "service_account": "564176696219-compute@developer.gserviceaccount.com",
+                "network_uri": "projects/tough-pod-475707-m1/global/networks/default",
+                "subnetwork_uri": "projects/tough-pod-475707-m1/regions/us-central1/subnetworks/default",
             }
         },
     }
@@ -79,7 +79,7 @@ with DAG(
         task_id="run_spark_job_on_dataproc_serverless",
         batch=batch_details,
         batch_id=batch_id,
-        project_id="psyched-service-442305-q1",
+        project_id="tough-pod-475707-m1",
         region="us-central1",
         gcp_conn_id="google_cloud_default",
     )
